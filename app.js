dc69534e3ea2b9a031d7746b0bad9bb06d7e7a9d
Angela Vogler <angelavogler4@gmail.com>
2017-08-16 16:15:08 -0400

    added original todo list that will add a new item

--- app.js
+++ app.js
@@ -15,20 +15,40 @@ app.set('view engine', 'mustache');  // what do we use to render them?
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // create array for tasks on todo liste
-const tasks = [
-  "walk dog",
-  "do laundry",
-  "cook meatballs"
+const todo = [
+  { task: 'walk dog' },
+  { task: 'do laundry' },
+  { task: 'cook meatballs' }
+];
+
+// create array for completed tasks
+const done = [
+  { task: 'eat lunch' }
 ];
 
 // display to do list
-app.get('/', function (req, req) {
+app.get('/', function (req, res) {
   res.render('todo', {
-    honeyDo: tasks
+    honeyDo: todo
   });
 });
 
 // receive new tasks (form info)
+app.post('/', function (req, res) {
+  if (
+    req.body.task.length != 0
+  ) {
+    todo.push({
+      task: req.body.task
+    })
+  }
+
+  // render new task item
+  res.render('todo', {
+    honeyDo: todo,
+    success: true
+  })
+});
 
 // create server
 app.listen(3003, function () { // 1024 and below are off limits
